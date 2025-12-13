028f8c103f6c2795e1ea72d5d7806ee33a0599f7
shivam patra <spworld30@gmail.com>
2025-12-13 13:37:49 +0530

    added_files.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return (
+        <header className = "bg-white shadow px-6 py-4 flex justify-between items-center">
+            <h1 className="text-lg font-semibold">Dashboard</h1>
+            <span className="text-gray-600">KIIT Authenticated</span>
+        </header>
+    )
+}
