b02a01d3ab1319e2342582eb1e80027d557c58b4
shivam patra <spworld30@gmail.com>
2025-12-13 13:30:28 +0530

    -_frontend_setup.

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,6 +33,6 @@ const PORT = process.env.PORT || 8080;
 
 connectDb().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port: ${PORT}`);
     });
 });
