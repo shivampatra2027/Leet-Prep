ad017e9184325a9472faa6509841023a8d9f0757
MD SAHIL <144834170+mdsahilnoob@users.noreply.github.com>
2025-12-26 02:46:54 +0530

    added middleware on /problems

--- backend/src/routes/problemRoutes.js
+++ backend/src/routes/problemRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getAllProblems,getCompanies,getProblemById} from "../controllers/problemController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 
-router.get("/", getAllProblems);
-router.get("/:id", getProblemById);
-router.get("/companies/:companyId", getProblemById);
-router.get("/companies", getCompanies);
+router.get("/",protect, getAllProblems);
+router.get("/:id",protect, getProblemById);
+router.get("/companies/:companyId", protect, getProblemById);
+router.get("/companies", protect, getCompanies);
 export default router;
