162e64a36a8ef199932a0df003e059a7ebf9c539
shivam patra <spworld30@gmail.com>
2025-12-27 01:39:31 +0530

    fix:allow non-kiit emails

--- backend/src/auth/google.js
+++ backend/src/auth/google.js
@@ -2,11 +2,7 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/User.js";
 
-
-// console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-// console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
-// console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
-console.log("Google OAuth is implemented..!")
+// console.log("Google OAuth is implemented..!")
 export function configureGoogleStrategy() {
     passport.use(
         new GoogleStrategy(
@@ -19,8 +15,8 @@ export function configureGoogleStrategy() {
                 try {
                     const email = profile.emails?.[0]?.value;
                     if (!email) return done(null, false, { message: "No email found" });
-                    if (!email.endsWith("@kiit.ac.in"))
-                        return done(null, false, { message: "Only KIIT email allowed" });
+                    // if (!email.endsWith("@kiit.ac.in"))
+                    //     return done(null, false, { message: "Only KIIT email allowed" });
 
                     let user = await User.findOne({ email });
                     if (!user) {
